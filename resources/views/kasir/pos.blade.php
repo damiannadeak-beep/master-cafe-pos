a7e1da83235c8b0503bdd8ddf293ba8ae794f784
Damian Nadeak <damiannadeak@gmail.com>
2026-09-09 10:03:06 +0700

    revert: kembalikan layout halaman pos kasir ke posisi semula yang pas dan presisi

--- resources/views/kasir/pos.blade.php
+++ resources/views/kasir/pos.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.kasir')
 
 @section('content')
-<div class="container-fluid px-0 h-100">
-    <div class="row gx-4 m-0 align-items-start h-100">
-        <div class="col-lg-8 col-md-7 h-100" style="overflow-y: auto; padding-right: 1rem;">
+<div class="container-fluid px-0" style="height: calc(100vh - 110px);">
+    <div class="row gx-4 m-0 align-items-start" style="height: 100%;">
+        <div class="col-lg-8 col-md-7" style="height: 100%; overflow-y: auto; padding-right: 1rem;">
             <x-pos.product-grid :menus="$menus" />
         </div>
-        <div class="col-lg-4 col-md-5 h-100" style="overflow-y: auto; border-left: 1px solid #21262d;">
+        <div class="col-lg-4 col-md-5" style="height: 100%; overflow-y: auto; border-left: 1px solid #21262d;">
             <x-pos.cart-sidebar :mejas="$mejas" :promos="$promos" />
         </div>
     </div>
@@ -17,15 +17,8 @@
 @include("components.kasir.pos-scripts")
 
 <style>
-    .kasir-main { 
-        padding: 1.25rem 1.5rem 1rem 1.5rem !important; 
-    }
-    .kasir-container { 
-        padding: 0 !important; 
-        overflow: hidden !important; 
-        max-width: 100% !important; 
-        height: 100% !important;
-    }
+    .kasir-main { padding: 0 !important; }
+    .kasir-container { padding: 0 !important; overflow: hidden !important; }
 
     .item-menu:hover { background-color: #F0E9DD; transform: translateY(-3px); border: 1px solid #3E2723 !important; }
 
