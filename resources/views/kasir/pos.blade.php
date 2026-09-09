8eb65061dbab45e56c6c00547d1a550286324893
Damian Nadeak <damiannadeak@gmail.com>
2026-09-09 10:06:20 +0700

    style(kasir): berikan margin-top 15px pada container pos kasir agar tidak menempel pada navbar

--- resources/views/kasir/pos.blade.php
+++ resources/views/kasir/pos.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.kasir')
 
 @section('content')
-<div class="container-fluid px-0" style="height: calc(100vh - 110px);">
+<div class="container-fluid px-0" style="height: calc(100vh - 125px); margin-top: 15px;">
     <div class="row gx-4 m-0 align-items-start" style="height: 100%;">
         <div class="col-lg-8 col-md-7" style="height: 100%; overflow-y: auto; padding-right: 1rem;">
             <x-pos.product-grid :menus="$menus" />
