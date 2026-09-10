a91c7fe9d06ac647245d29e9e4521868cbe46df8
Damian Nadeak <damiannadeak@gmail.com>
2026-09-10 11:34:00 +0700

    fix: add missing Log facade import in OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use App\Models\{Pesanan, DetailPesanan, Pembayaran, Menu, Meja, Promo, Setting};
 use App\Http\Requests\Konsumen\{TambahPesananRequest, CallBellRequest};
 use App\Services\OrderService;
